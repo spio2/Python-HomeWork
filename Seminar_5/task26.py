36be8df9448511a8495d7adc9505571e467affbe
LiAnna <spio@mail.ru>
2023-03-06 18:02:30 +0300

    добавила решения Семинар 5

--- Seminar_5/task26.py
+++ Seminar_5/task26.py
@@ -5,3 +5,12 @@
 # A = 2; B = 3 -> 8 
 
 
+def recApowB(a, b):
+    if b == 0:
+        return 1
+    return a * recApowB(a, b - 1)
+
+a = int(input('Введите число: '))
+b = int(input('Введите степень: '))
+
+print(recApowB(a, b))
--- Seminar_5/task28.py
+++ Seminar_5/task28.py
@@ -4,3 +4,13 @@
 # *Пример:*
 # 2 2
 # 4 
+
+a = int(input('Введите число А: '))
+b = int(input('Введите число В: '))
+
+def res_sum(a, b):
+    if b == 0:
+        return a
+    return res_sum(a + 1, b - 1)
+
+print(res_sum(a, b))
